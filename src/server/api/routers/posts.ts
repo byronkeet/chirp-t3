b5ef3124f4b3f4bd729c18de7b43b1fca50b5027
Byron Keet <keetbis@gmail.com>
2023-07-05 17:24:37 +0200

    Rate limit the endpoint

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,6 +3,8 @@ import type { User } from "@clerk/nextjs/dist/types/api";
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
+import { Redis } from "@upstash/redis";
 
 const filterUserForClient = (user: User) => {
 	return {
@@ -12,6 +14,13 @@ const filterUserForClient = (user: User) => {
 		firstName: user.firstName,
 	};
 };
+
+// Create a new ratelimiter, that allows 3 requests per 1 minute
+const ratelimit = new Ratelimit({
+	redis: Redis.fromEnv(),
+	limiter: Ratelimit.slidingWindow(3, "1 m"),
+	analytics: true,
+});
 
 export const postsRouter = createTRPCRouter({
 	getAll: publicProcedure.query(async ({ ctx }) => {
@@ -51,6 +60,11 @@ export const postsRouter = createTRPCRouter({
 		)
 		.mutation(async ({ ctx, input }) => {
 			const authorId = ctx.userId;
+			const { success } = await ratelimit.limit(authorId);
+			if (!success) throw new TRPCError({
+				code: "TOO_MANY_REQUESTS",
+				message: "You are doing that too much.",
+			});
 			const post = await ctx.prisma.post.create({
 				data: {
 					authorId,
